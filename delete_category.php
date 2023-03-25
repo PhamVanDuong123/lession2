644a84b0e3a2bff4abbe56cb79b50e8a8a661e03
PhamVanDuong123 <72380296+PhamVanDuong123@users.noreply.github.com>
2023-03-25 14:10:22 +0700

    update code
    update readme

--- delete_category.php
+++ delete_category.php
@@ -4,24 +4,24 @@ if($_POST){
   
     // include database and object file
     include_once 'config/database.php';
-    include_once 'objects/category.php';
+    include_once 'model/category.php';
   
     // get database connection
     $database = new Database();
     $db = $database->getConnection();
   
     // prepare product object
-    $product = new Category($db);
+    $category = new Category($db);
       
-    // set product id to be deleted
-    $product->id = $_POST['object_id'];
+    // set category id to be deleted
+    $category->id = $_POST['object_id'];
       
-    // delete the product
-    if($product->delete()){
+    // delete the category
+    if($category->delete()){
         echo "Object was deleted.";
     }
       
-    // if unable to delete the product
+    // if unable to delete the category
     else{
         echo "Unable to delete object.";
     }
--- update_category.php
+++ update_category.php
@@ -3,65 +3,59 @@ $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: missing ID.');
 
 // include database and object files
 include_once 'config/database.php';
-// include_once 'objects/product.php';
-include_once 'objects/category.php';
+
+include_once 'model/category.php';
 
 // get database connection
 $database = new Database();
 $db = $database->getConnection();
 
 // prepare objects
-// $parent = new parent($db);
+
 $category = new Category($db);
-
-
 $category->id = $id;
-
-
 $category->readOne();
-// set page header
-$page_title = "Update Product";
-include_once "layout_header.php";
-echo "<div class='right-button-margin' style='marign:50px'>
-         
 
+// set page header
+$page_title = "Update Category";
+include_once "view/layout_header.php";
+echo "<div class='right-button-margin' style='margin-bottom:20px'>
      </div>";
      if($_POST){
   
-        // set product property values
+        // set category property values
         $category->name = $_POST['name'];
      
         $category->parent_id = $_POST['parent_id'];
       
-        // update the product
+        // update the category
         if($category->update()){
             echo "<div class='alert alert-success alert-dismissable'>";
-                echo "Product was updated.";
+                echo "category was updated.";
             echo "</div>";
         }
       
-        // if unable to update the product, tell the user
+        // if unable to update the category, tell the user
         else{
             echo "<div class='alert alert-danger alert-dismissable'>";
-                echo "Unable to update product.";
+                echo "Unable to update category.";
             echo "</div>";
         }
     }
     ?>
-    <!-- post code will be here -->
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post">
-        <table class='table table-hover table-responsive table-bordered'>
-    
-            <tr>
-                <td>Name</td>
-                <td><input type='text' name='name' value='<?php echo $category->name; ?>' class='form-control' /></td>
-            </tr>
-    
-         
-            <tr>
-                <td>Category</td>
-                <td>
-                    <?php
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post">
+    <table class='table table-hover table-responsive table-bordered'>
+
+        <tr>
+            <td>Name</td>
+            <td><input type='text' name='name' value='<?php echo $category->name; ?>' class='form-control' /></td>
+        </tr>
+
+
+        <tr>
+            <td>Category</td>
+            <td>
+                <?php
                     $stmt = $category->read();
     
                     // put them in a select drop-down
@@ -72,7 +66,7 @@ echo "<div class='right-button-margin' style='marign:50px'>
                         $parent_id = $row_category['id'];
                         $category_name = $row_category['name'];
     
-                        // current category of the product must be selected
+                        // current category of the category must be selected
                         if ($category->parent_id == $parent_id) {
                             echo "<option value='$parent_id' selected>";
                         } else {
@@ -83,23 +77,22 @@ echo "<div class='right-button-margin' style='marign:50px'>
                     }
                     echo "</select>";
                     ?>
-                </td>
-            </tr>
-    
-            <tr>
-                <td></td>
-                <td>
-                    <button type="submit" class="btn btn-primary">Update</button>
-                    <a href='index.php' class='btn btn-default pull-right'>Back</a>
-                </td>
-            </tr>
-    
-        </table>
-    </form>
+            </td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td>
+                <button type="submit" class="btn btn-primary">Update</button>
+                <a href='index.php' class='btn btn-default pull-right'>Back</a>
+            </td>
+        </tr>
+
+    </table>
+</form>
 
 
 
 <?php 
-include_once "layout_footer.php";
+include_once "view/layout_footer.php";
 ?>
-
